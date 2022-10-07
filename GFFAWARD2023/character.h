2e0b9c428c041d079cae10446b288da85678c81d
yusaku-uema <106945257+yusaku-uema@users.noreply.github.com>
2022-10-07 15:00:39 +0900

    10/7日 CharacterのCPPとhファイルを作った。

---
+++ GFFAWARD2023/character.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Character
+{
+private:
+	float x, y;//横ｘ、縦y
+	float speed; //少年の移動速度
+	int CharacterImages; //少年のイメージ画像
+public:
+	
+	
+};
+
+
+ 
--- GFFAWARD2023/main.cpp
+++ GFFAWARD2023/main.cpp
@@ -2,17 +2,22 @@
 
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-
-	SetOutApplicationLogValidFlag(FALSE);   //ログ出力を無効にする
-	SetMainWindowText("GFF");
-	ChangeWindowMode(TRUE);
-	SetWindowSize(1280, 720);
+    // タイトル
+    SetMainWindowText("GFF");
+	ChangeWindowMode(TRUE);// ウィンドウモードで起動
+	SetWindowSize(1280, 720); //画面サイズ
+    if (DxLib_Init() == -1) return -1;	// DXライブラリの初期化処理
 	
-	SetDrawScreen(DX_SCREEN_BACK);
-
+	SetDrawScreen(DX_SCREEN_BACK);// 描画先画面を裏にする
+    // ゲームループ
 	while (ProcessMessage() == 0) {
+        ClearDrawScreen();		// 画面の初期化
 
-	
+		ScreenFlip();			// 裏画面の内容を表画面に反映
+
+		DxLib_End();	// DXライブラリ使用の終了処理
+
 		return 0;
 	}
+
 }
